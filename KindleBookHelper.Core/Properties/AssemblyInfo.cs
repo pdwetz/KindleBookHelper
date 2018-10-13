83e0afd8d4d70f55f1bba4f24ead2c0a7a27e5bd
Peter Wetzel <pdwetz@gmail.com>
2018-10-13 00:22:48 -0400

    Updated to latest .net framework

--- KindleBookHelper.Core/Properties/AssemblyInfo.cs
+++ KindleBookHelper.Core/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("KindleBookHelper")]
-[assembly: AssemblyCopyright("Copyright ©  2016 Peter Wetzel")]
+[assembly: AssemblyCopyright("Copyright ©  2018 Peter Wetzel")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("3b63a76a-c55e-4cae-84c4-d07a2cbc1afe")]
 
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
--- KindleBookHelper.Test/Properties/AssemblyInfo.cs
+++ KindleBookHelper.Test/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("KindleBookHelper")]
-[assembly: AssemblyCopyright("Copyright ©  2016 Peter Wetzel")]
+[assembly: AssemblyCopyright("Copyright ©  2018 Peter Wetzel")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("7ae3e0e5-19b9-4927-bfd6-d2c88f536805")]
 
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
