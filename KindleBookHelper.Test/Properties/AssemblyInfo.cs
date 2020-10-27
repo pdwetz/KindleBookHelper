ced6c14bfa46079e8b2b815792f057ad386eac35
Peter Wetzel <pdwetz@gmail.com>
2020-10-26 22:31:30 -0400

    version bump + removal of reference to kindlegen.

--- KindleBookHelper.Test/Properties/AssemblyInfo.cs
+++ KindleBookHelper.Test/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("KindleBookHelper")]
-[assembly: AssemblyCopyright("Copyright ©  2019 Peter Wetzel")]
+[assembly: AssemblyCopyright("Copyright ©  2020 Peter Wetzel")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("7ae3e0e5-19b9-4927-bfd6-d2c88f536805")]
 
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.1.1.0")]
+[assembly: AssemblyFileVersion("2.1.1.0")]
--- KindleBookHelper/Properties/AssemblyInfo.cs
+++ KindleBookHelper/Properties/AssemblyInfo.cs
@@ -2,16 +2,16 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("KindleBookHelper")]
-[assembly: AssemblyDescription("Converts raw text file to html format that can be consumed by KindleGen or Kindle Previewer.")]
+[assembly: AssemblyDescription("Converts raw text file to html format that can be consumed by Kindle Previewer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("KindleBookHelper")]
-[assembly: AssemblyCopyright("Copyright ©  2019 Peter Wetzel")]
+[assembly: AssemblyCopyright("Copyright ©  2020 Peter Wetzel")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("dc2d43bd-99a4-4644-bb22-b7288ac659c2")]
 
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.1.1.0")]
+[assembly: AssemblyFileVersion("2.1.1.0")]
